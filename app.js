a8af985b4a2f3817b9cf1a53096ecf99b4a94758
Rob Torres <robetorres@paypal.com>
2023-11-26 14:52:12 -0600

    3 files commit

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+// Initialize Firebase
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js';
+import { getFirestore, collection, addDoc, getDocs, updateDoc, doc } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js';
+
+const firebaseConfig = {
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    appId: 'YOUR_APP_ID'
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 document.addEventListener('DOMContentLoaded', function () {
     loadAnswers();
 });
@@ -5,55 +21,73 @@ document.addEventListener('DOMContentLoaded', function () {
 function submitForm() {
     const name = document.getElementById('name').value;
     const country = document.getElementById('country').value;
+    const share = document.getElementById('share').value;
 
-    if (name && country) {
-        postToGoogleSheet(name, country);
+    if (name && country && share) {
+        saveToFirebase(name, country, share);
         clearForm();
         loadAnswers();
     } else {
-        alert('Please enter both your name and country.');
+        alert('Please enter all fields.');
     }
 }
 
-function postToGoogleSheet(name, country) {
-    // You need to replace 'YOUR_GOOGLE_SCRIPT_URL' with the URL of your Google Apps Script
-    const scriptUrl = 'https://script.google.com/home/projects/13RaTOi97UCgS-cMQGjVpll5fXRvPZdsd9Ogu6g1aXsv7jwJGrufnASZw/edit';
+async function saveToFirebase(name, country, share) {
+    try {
+        const docRef = await addDoc(collection(db, 'travelers'), {
+            name: name,
+            country: country,
+            share: share
+        });
 
-    const formData = new FormData();
-    formData.append('name', name);
-    formData.append('country', country);
-
-    fetch(scriptUrl, {
-        method: 'POST',
-        body: formData,
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
+        console.log('Document written with ID: ', docRef.id);
+    } catch (error) {
+        console.error('Error adding document: ', error);
+    }
 }
 
-function loadAnswers() {
-    // You need to replace 'YOUR_GOOGLE_SHEET_URL' with the URL of your Google Sheet
-    const sheetUrl = 'https://docs.google.com/spreadsheets/d/1-XSJYcsD3JP4Bf1XpFo6Ef2cj561T_SWj2KN5qFlqDQ';
-
-    fetch(sheetUrl)
-        .then(response => response.json())
-        .then(data => displayAnswers(data))
-        .catch(error => console.error('Error:', error));
-}
-
-function displayAnswers(answers) {
+async function loadAnswers() {
     const answersList = document.getElementById('answersList');
     answersList.innerHTML = '';
 
-    answers.forEach(answer => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${answer.name} - ${answer.country}`;
-        answersList.appendChild(listItem);
-    });
+    try {
+        const querySnapshot = await getDocs(collection(db, 'travelers'));
+
+        querySnapshot.forEach((doc) => {
+            const answer = doc.data();
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `<strong>${answer.name}</strong> - ${answer.country} - ${answer.share} <button onclick="updateAnswer('${doc.id}')">Update</button>`;
+            answersList.appendChild(listItem);
+        });
+    } catch (error) {
+        console.error('Error getting documents: ', error);
+    }
 }
 
 function clearForm() {
     document.getElementById('name').value = '';
     document.getElementById('country').value = '';
+    document.getElementById('share').value = '';
 }
+
+async function updateAnswer(docId) {
+    const newName = prompt('Enter new name:');
+    const newCountry = prompt('Enter new country:');
+    const newShare = prompt('Enter new share item:');
+
+    if (newName !== null && newCountry !== null && newShare !== null) {
+        try {
+            await updateDoc(doc(db, 'travelers', docId), {
+                name: newName,
+                country: newCountry,
+                share: newShare
+            });
+
+            console.log('Document successfully updated!');
+            loadAnswers();
+        } catch (error) {
+            console.error('Error updating document: ', error);
+        }
+    }
+}
+
